0dd1a38186cbb3d63cfd1380bc0016bae0a13921
Adrian Nads <adrian.marcelo.pixel8@gmail.com>
2023-03-27 00:07:37 +0800

    ADDED Users mongodb schema

---
+++ models/Users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        minLength: 8,
+        required: true
+    },
+    password: {
+        type: String,
+        minLength: 8,
+        required: true
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
