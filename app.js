68ec2be14e7c7c4c85b9121e7e990b98d54fd713
Adrian Nads <adrian.marcelo.pixel8@gmail.com>
2023-05-12 16:23:28 +0800

    ADDED origin in cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 });
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: 'https://yanyan-store.netlify.app' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
